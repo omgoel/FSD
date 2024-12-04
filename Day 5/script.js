97f9e503f1e7d0759b6f3a95a03f68e4649d33e1
Om Goel <65081768+omgoel@users.noreply.github.com>
2024-12-04 14:17:37 +0530

    Add files via upload

---
+++ Day 5/script.js
@@ -0,0 +1,49 @@
+// function display() {
+//   alert("Hello JS");
+// }
+// let x=12.23;
+// let y="hello";
+// let z=true;
+// let obj={
+//   'name': 'Rahul',
+//   id: 12,
+// }
+// let ar=[12,2,3,4,4];
+// let a;
+// let b=null;
+// console.log(typeof b);
+
+// function validate() {
+//   let un = document.getElementById("un").value;
+//   let pass = document.getElementById("pass").value;
+//   if (un == "admin" && pass == "admin")
+//     document.getElementById("res").innerHTML = "Login Success";
+//   else
+//   document.getElementById("res").innerHTML = "Login Failed";
+// }
+// "use strict"
+// let num=12;
+// let x=Number("12");
+// let str=Number("123");
+// let b=Boolean(12);
+// console.log('12'+'3');
+// console.log(num);
+
+// let a=12;
+// let b='12';
+// console.log(a==b);
+// console.log(a===b);
+
+// let a = (x,y) => {
+//   return (x+y);
+// };
+
+// let x=a(12,34);
+// console.log(x);
+//  let say=()=> console.log("Hello");
+//  say();
+function display() {
+    let p1 = document.querySelector("p");
+    p1.innerHTML = "Result";
+    p1.style.backgroundColor = "red";
+  }
